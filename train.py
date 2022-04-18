3f5805eeabd6bcf3cabbfdf0e49aa49b3f4cfe54
jacobthebanana <jacob@banana.abay.cf>
2022-04-18 11:36:04 +0000

    Implemented training on wikidata text.

--- train.py
+++ train.py
@@ -1,8 +1,10 @@
 import math
+import argparse
 
 import torch
 from torch.utils.tensorboard import SummaryWriter
 from tqdm.auto import tqdm
+import datasets
 
 import data_utils
 from model import TransformerModel
@@ -20,13 +22,27 @@ test_ratio = 0.3
 device = torch.device("cuda")
 writer = SummaryWriter(filename_suffix=f"batch_{batch_size}")
 
+parser = argparse.ArgumentParser()
+parser.add_argument("dataset_path")
+args = parser.parse_args()
 
-X, embeddings = data_utils.load_embedding_file(data_file, device=device, limit=-1)
-X, embeddings = data_utils.shuffle(X, embeddings)
-X_train, Y_train, X_test, Y_test = data_utils.split(X, embeddings, train_ratio)
+dataset_path = args.dataset_path
+dataset = datasets.load_from_disk(dataset_path)
 
+embedding_file_path = "data/glove.6B.100d.txt"
+_, embeddings, word_lookup, word_list = data_utils.load_embedding_file(
+    embedding_file_path, limit=-1
+)
 output_dimension = embeddings.shape[1]
-num_words = X.shape[0]
+
+train_length = len(dataset["train"])
+actual_train_length = train_length // 17
+
+train_dataloader = torch.utils.data.DataLoader(
+    dataset["train"][:actual_train_length], batch_size=batch_size
+)
+test_dataloader = torch.utils.data.DataLoader(dataset["test"], batch_size=batch_size)
+
 
 model = TransformerModel(
     input_dimension=input_dimension,
@@ -41,56 +57,46 @@ criterion = torch.nn.MSELoss()
 optimizer = torch.optim.AdamW(model.parameters())
 
 for epoch in tqdm(range(n_epoch)):
-    num_iterations = math.ceil((num_words * train_ratio) / batch_size)
-
     model.train()
     train_loss_tally = 0
-    for iteration in range(num_iterations):
-        start_index = batch_size * iteration
-        X_batch, X_length_mask, Y_batch = data_utils.get_batch(
-            X_train,
-            Y_train,
-            start=start_index,
-            batch_size=batch_size,
-            device=device,
+    words_processed = 0
+
+    for words in tqdm(train_dataloader):
+        X, X_length_mask, Y = data_utils.prepare_batch(
+            words, embeddings, word_lookup, device=device
         )
 
-        Y_predictions = model(X_batch, length_mask=X_length_mask)
-        loss = criterion(Y_batch, Y_predictions)
+        Y_predictions = model(X, length_mask=X_length_mask)
+        loss = criterion(Y, Y_predictions)
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
         train_loss_tally += loss
+        words_processed += Y.shape[0]
 
-    train_loss_tally = train_loss_tally / train_ratio
+    train_loss_tally = train_loss_tally / words_processed
     writer.add_scalar("Loss/Train", train_loss_tally, epoch)
-
-    num_iterations = math.ceil((num_words * test_ratio) / batch_size)
 
     model.eval()
     test_loss_tally = 0
-    for iteration in range(num_iterations):
-        start_index = batch_size * iteration
-        X_batch, X_length_mask, Y_batch = data_utils.get_batch(
-            X_test,
-            Y_test,
-            start=start_index,
-            batch_size=batch_size,
-            device=device,
+    words_processed = 0
+    for words in tqdm(test_dataloader):
+        X, X_length_mask, Y = data_utils.prepare_batch(
+            words, embeddings, word_lookup, device=device
         )
 
         with torch.no_grad():
-            Y_predictions = model(X_batch, length_mask=X_length_mask)
-            loss = criterion(Y_batch, Y_predictions)
+            Y_predictions = model(X, length_mask=X_length_mask)
+            loss = torch.sum(criterion(Y, Y_predictions, axis=0))
             test_loss_tally += loss
+            words_processed += Y.shape[0]
 
-    test_loss_tally = test_loss_tally / test_ratio
+    test_loss_tally = test_loss_tally / words_processed
     writer.add_scalar("Loss/Test", test_loss_tally, epoch)
     print(
         f"{epoch+1}: Train loss {train_loss_tally:.5f}, Test loss {test_loss_tally:.5f}"
     )
 
-    if epoch == n_epoch or epoch % 9 == 0:
-        torch.save(model.state_dict(), f"models/batch_{batch_size}_{epoch}.pth")
+    torch.save(model.state_dict(), f"models/batch_{batch_size}_{epoch}.pth")
